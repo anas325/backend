22f1b95a1fd1492687f3eeb076920a2216173c6c
anas325 <91235726+anas325@users.noreply.github.com>
2025-04-04 09:38:11 +0000

    Add initial Django project structure with events and users apps

---
+++ backend/apps/users/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to="profile_pics/", blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
