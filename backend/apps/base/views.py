a237b94f8165b720ccaaadc6bdddfc50e75cad06
anas325 <91235726+anas325@users.noreply.github.com>
2025-04-06 16:19:38 +0100

    changed background color

--- backend/apps/base/views.py
+++ backend/apps/base/views.py
@@ -1,9 +1,7 @@
-
-
 # Create your views here.
 from django.shortcuts import render
 from django.views import View
 
 class home(View):
     def get(self, request):
-        return render(request, 'home.html',{})
+        return render(request, 'home.html', {'first_name': request.user.first_name if request.user.is_authenticated else ''})
