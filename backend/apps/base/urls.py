7447a80fc45f7e252afa95e501572f89acd51714
mehdiezzahraoui35@gmail.com <you@example.com>
2025-04-07 16:54:48 +0100

    Refactor templates and views for improved organization and maintainability

--- backend/apps/base/urls.py
+++ backend/apps/base/urls.py
@@ -4,6 +4,10 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home.as_view(), name='home'),
+    path('', views.about.as_view(), name='about'),
+    path('user_profile/', views.user_profile.as_view(), name='user_profile'),
+    path('events/', views.events.as_view(), name='events'),
+    path('create_event/', views.create_event.as_view(), name='create_event'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('user/<int:id>/', views.profile.as_view(), name='user_profile'),
--- backend/apps/base/views.py
+++ backend/apps/base/views.py
@@ -5,9 +5,18 @@ from django.shortcuts import render
 from django.views import View
 from backend.apps.users.models import User
 
-class home(View):
+class home1(View):
     def get(self, request):
         return render(request, 'home1.html',{})
+class events(View):
+    def get(self, request):
+        return render(request, 'events.html',{})
+class create_event(View):
+    def get(self, request):
+        return render(request, 'create_event.html',{})
+class user_profile(View):
+    def get(self, request):
+        return render(request, 'user_profile.html',{})
 
 
 class profile(View):
